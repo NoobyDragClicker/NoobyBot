bd46831dea34e1098e97de0f0b4d5f891e616a56
NoobyDragClicker <spencerburwell99@gmail.com>
2026-01-30 10:20:31 -0500

    Separate SEE from search | bench 4835894

--- src/AI/Search.cs
+++ src/AI/Search.cs
@@ -49,7 +49,6 @@ public class Search
     //SEE
     const int SEE_QUIET_MARGIN = -100;
     const int SEE_NOISY_MARGIN = -50;
-    int[] SEEPieceVals = [0, 100, 300, 300, 500, 900, 0];
 
     public Search(Board board, AISettings aiSettings, SearchLogger logger)
     {
@@ -279,7 +278,7 @@ public class Search
 
             //SEE pruning
             int seeMargin = isTactical ? SEE_NOISY_MARGIN * depth : SEE_QUIET_MARGIN * depth;
-            if(depth < 5 && !SEE(currentMove, seeMargin)){ continue; }
+            if(depth < 5 && !SEE.EvaluateSEE(board, currentMove, seeMargin)){ continue; }
 
 
             board.MakeMove(currentMove, true);
@@ -407,7 +406,7 @@ public class Search
             if(moveNum == numMoves - 1){ stage++; }
 
             //Skip a move with a bad SEE
-            if(!SEE(currentMove, 0)){ continue; }
+            if(!SEE.EvaluateSEE(board, currentMove, 0)){ continue; }
 
             //Delta pruning
             if ((standPat + GetCapturedPieceVal(currentMove) + QS_DELTA_PRUNING_MARGIN) < alpha){ continue; }
@@ -462,88 +461,6 @@ public class Search
         if (isInCheck) { return false; }
         if (fullMoveClock > 1 && staticEvals[fullMoveClock - 2] != NEGATIVE_INFINITY) { return staticEvals[fullMoveClock] > staticEvals[fullMoveClock - 2]; }
         return true;
-    }
-
-    bool SEE(Move move, int threshold)
-    {
-        //Implementation from ethereal
-        int nextVictim = move.isPromotion() ? move.PromotedPieceType() : board.MovedPieceType(move);
-        int balance = EstimatedCaptureValue(move) - threshold;
-
-        //Capture is not worth the threshold
-        if(balance < 0){ return false; }
-        
-        //If the moved piece is captured and we are still better, it is a good capture
-        balance -= SEEPieceVals[nextVictim];
-        if(balance >= 0){ return true; }
-
-        ulong bishops = board.pieceBitboards[Board.PieceBitboardIndex(Board.WhiteIndex, Piece.Bishop)] | board.pieceBitboards[Board.PieceBitboardIndex(Board.BlackIndex, Piece.Bishop)] | board.pieceBitboards[Board.PieceBitboardIndex(Board.WhiteIndex, Piece.Queen)] | board.pieceBitboards[Board.PieceBitboardIndex(Board.BlackIndex, Piece.Queen)];
-        ulong rooks = board.pieceBitboards[Board.PieceBitboardIndex(Board.WhiteIndex, Piece.Rook)] | board.pieceBitboards[Board.PieceBitboardIndex(Board.BlackIndex, Piece.Rook)] | board.pieceBitboards[Board.PieceBitboardIndex(Board.WhiteIndex, Piece.Queen)] | board.pieceBitboards[Board.PieceBitboardIndex(Board.BlackIndex, Piece.Queen)];
-
-        //Update occupancy
-        ulong allPieces = board.allPiecesBitboard;
-        allPieces = (allPieces ^ (1ul<<move.oldIndex)) | (1ul<<move.newIndex);
-        if(move.flag == Move.EnPassant){ allPieces ^= 1ul<<board.enPassantIndex; }
-
-        ulong attackers = board.GetAttackersToSquare(move.newIndex, allPieces, rooks, bishops) & allPieces;
-
-        int currentColorIndex = board.oppositeColorIndex;
-
-        ulong myAttackers;
-        while (true)
-        {
-            myAttackers = attackers & board.sideBitboard[currentColorIndex];
-            if(myAttackers == 0){ break; }
-            for(nextVictim = Piece.Pawn; nextVictim <= Piece.Queen; nextVictim++)
-            {
-                if((myAttackers & board.pieceBitboards[Board.PieceBitboardIndex(currentColorIndex, nextVictim)]) != 0){ break; }
-            }
-
-
-            //Update occupancy
-            allPieces ^= (1ul << BitOperations.TrailingZeroCount(myAttackers & board.pieceBitboards[Board.PieceBitboardIndex(currentColorIndex, nextVictim)]));
-
-            //A diagonal move can reveal a bishop or a queen attacker
-            if(nextVictim == Piece.Pawn || nextVictim == Piece.Bishop || nextVictim == Piece.Queen)
-            {
-                attackers |= BitboardHelper.GetBishopAttacks(move.newIndex, allPieces) & bishops;
-            }
-            //Rook or queen move can reveal a rook or queen attacker
-            if(nextVictim == Piece.Rook || nextVictim == Piece.Queen)
-            {
-                attackers |= BitboardHelper.GetRookAttacks(move.newIndex, allPieces) & rooks;
-            }
-
-            //Remove any already used attacks
-            attackers &= allPieces;
-
-            currentColorIndex = 1 - currentColorIndex;
-
-            balance = -balance - 1 - SEEPieceVals[nextVictim];
-
-            if(balance >= 0)
-            {
-                //If our last attacking piece is a king, and the opponent has attackers, we have lost as the move we followed would be illegal
-                if(nextVictim == Piece.King && (attackers & board.sideBitboard[currentColorIndex]) != 0)
-                {
-                    currentColorIndex = 1 - currentColorIndex;
-                }
-                break;
-            }
-        }
-        return board.currentColorIndex != currentColorIndex;
-    }
-
-    int EstimatedCaptureValue(Move move)
-    {
-        if(move.flag == Move.EnPassant){ return SEEPieceVals[Piece.Pawn]; }
-        else if (move.isPromotion()){
-            return SEEPieceVals[board.PieceAt(move.newIndex)] + SEEPieceVals[move.PromotedPieceType()] - SEEPieceVals[Piece.Pawn];
-        }
-        else
-        {
-            return SEEPieceVals[board.PieceAt(move.newIndex)];
-        }
     }
 
     public static bool IsMateScore(int score)
